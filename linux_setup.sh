f241de8a7417396491c51261876cef49896db5b0
fistaco <fiske_schijlen@hotmail.com>
2026-02-07 18:49:46 +0100

    Add Linux setup script

---
+++ linux_setup.sh
@@ -0,0 +1,23 @@
+sudo apt install -yy python3-dev ghidra autojump openjdk-21-jdk zsh-autosuggestions zsh-syntax-highlighting jq fd-find npm nodejs strace gdb python3-pwntools python3-numpy sshuttle feroxbuster gobuster wget seclists python3-cryptodome xh zoxide pandoc poppler-utils ffmpeg dust wine64 wine32 winetricks certipy-ad ligolo-ng chisel trivy keepassxc-minimal 
+# Download ripgrep-all: https://github.com/phiresky/ripgrep-all/releases
+
+# Install uv for Python package management
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+ln -s $(which fdfind) ~/.local/bin/fd
+source /usr/share/autojump/autojump.zsh
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
+cargo install tealdeer ripgrep bat
+
+# Download/install tools from sources
+wget https://github.com/SpecterOps/bloodhound-cli/releases/latest/download/bloodhound-cli-linux-amd64.tar.gz  # bloodhound-cli install
+curl -fsSL https://raw.githubusercontent.com/opengrep/opengrep/main/install.sh | bash  # opengrep
+# git clone https://github.com/opengrep/opengrep-rules
+
+
+# Download VS Code
+# Download obsidian
+
+echo 'ROCKYOU=/usr/share/wordlists/rockyou.txt' >> ~/.zshrc
